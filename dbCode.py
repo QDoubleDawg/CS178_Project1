8610c1add07b61a382e49ac285530ae643b12096
EC2 Default User <ec2-user@ip-172-31-94-157.ec2.internal>
2025-04-10 20:43:08 +0000

    created the dp code to run querys

---
+++ dbCode.py
@@ -0,0 +1,29 @@
+import pymysql
+import pymysql.cursors
+import creds 
+
+def get_conn():
+    """establishes and returns a connection to the MySQL RDS database
+    using credentials from my creds.py. uses DictCursor to return 
+     query results as dictionaries!!!! """
+    
+    return pymysql.connect(
+        host=creds.host,
+        user=creds.user,
+        password=creds.password,
+        db=creds.db,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+def execute_query(query, args=()):
+    """executes an sql query using a conneciton to my rds database
+    closes the connection after running the query and returns the results 
+    in a list of dictionaries """
+
+    conn = get_conn()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query, args)
+            return cur.fetchall()
+    finally:
+        conn.close()
